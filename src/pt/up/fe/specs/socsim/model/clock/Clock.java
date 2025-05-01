f503f208fc16e7e5b45c2795bd62c31e4b9f6922
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2025-05-01 11:12:35 +0100

    Implemented clock/reset signals

--- src/pt/up/fe/specs/socsim/model/clock/Clock.java
+++ src/pt/up/fe/specs/socsim/model/clock/Clock.java
@@ -1,5 +1,18 @@
 package pt.up.fe.specs.socsim.model.clock;
 
-import pt.up.fe.specs.socsim.model.Direction;
+import pt.up.fe.specs.socsim.model.signal.IOType;
+import pt.up.fe.specs.socsim.model.signal.Signal;
 
-public record Clock(String name, Direction direction, Edge edge) { }
+public class Clock extends Signal {
+    private final Edge edge;
+
+    public Clock(String name, IOType io, Edge edge) {
+        super(name, io);
+
+        this.edge = edge;
+    }
+
+    public Edge getEdge() {
+        return this.edge;
+    }
+}
--- src/pt/up/fe/specs/socsim/model/signal/Signal.java
+++ src/pt/up/fe/specs/socsim/model/signal/Signal.java
@@ -1,5 +1,15 @@
 package pt.up.fe.specs.socsim.model.signal;
 
 public abstract class Signal {
-    private String name;
+    private final String name;
+    private final IOType io;
+
+    public Signal(String name, IOType io) {
+        this.name = name;
+        this.io = io;
+    }
+
+    public String getName() { return this.name; }
+
+    public IOType getIOType() { return this.io; }
 }
