666d1d07b2a936bab6936e4f65c16b24bdf74d6d
Pedro Ramalho <pedrocasramalhocas@gmail.com>
2025-05-01 10:29:38 +0100

    Implement a simple reset signal package

---
+++ src/pt/up/fe/specs/socsim/model/reset/Active.java
@@ -0,0 +1,26 @@
+package pt.up.fe.specs.socsim.model.reset;
+
+public enum Active {
+    HIGH("high"),
+    LOW("low");
+
+    private final String active;
+
+    Active(String active) {
+        this.active = active;
+    }
+
+    public String getActive() {
+        return this.active;
+    }
+
+    public static Active fromString(String active) {
+        for (Active value : Active.values()) {
+            if (value.getActive().equals(active)) {
+                return value;
+            }
+        }
+
+        throw new IllegalArgumentException("Invalid active string: " + active);
+    }
+}
---
+++ src/pt/up/fe/specs/socsim/model/reset/Reset.java
@@ -0,0 +1,6 @@
+package pt.up.fe.specs.socsim.model.reset;
+
+import pt.up.fe.specs.socsim.model.Direction;
+
+public record Reset(String name, Direction direction) {
+}
